b4161fb5edb7caa86b674843d6cee35fea7aaaa9
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2023-08-03 18:58:44 +0300

    gat api ready

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+import {getAllCategories, getTopBooks, getSeparateCategories, getBookInfo} from "./js/api"
+
+getAllCategories()
+getTopBooks()
+getSeparateCategories("Business Books")
+getBookInfo("643282b1e85766588626a0b6")
