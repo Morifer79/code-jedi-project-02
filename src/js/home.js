2d1f235467bcd173b7f8f6d7b7c7e6838dc20c08
ilesyk <pavloyavorskyi@ukr.net>
2023-08-03 19:15:08 +0300

    updated home.css

--- src/js/home.js
+++ src/js/home.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 const container = document.querySelector('.js-home-markup');
 
 const HOME_REQ = 'https://books-backend.p.goit.global/books/top-books';
@@ -15,20 +15,20 @@ function createMarkupCards(data) {
   const markup = data
     .map(
       data => `
-    <div>
-      <h3>${data.list_name}</h3>
+    <div class="home-card-container">
+      <h3 class="home-category-title">${data.list_name}</h3>
       <ul class="home-category-cards">
       ${data.books
         .map(
-          book => `<li class="book-card" id="${book._id}">
-        <img src="${book.book_image}" alt="${book.title} class="home-card-img" loading="lazy" width="335">
-        <h2>${book.title}</h2>
-        <p>${book.author}</p>
+          book => `<li class="home-card" id="${book._id}">
+        <img src="${book.book_image}" alt="${book.title} loading="lazy" width="335">
+        <h2 class="home-card-title">${book.title}</h2>
+        <p class="home-card-author">${book.author}</p>
       </li>`
         )
         .join('')}
       </ul>
-      <button type="button">see more</button>
+      <button type="button" class="home-category-btn">see more</button>
     </div>`
     )
     .join('');
