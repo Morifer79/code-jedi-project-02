d8a0d26e4936c6751164cbea2076ad68836c64a4
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2023-08-04 13:22:29 +0300

    Убрал вызовы функций з index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-import axios from 'axios';
-
 import {getAllCategories, getTopBooks, getSeparateCategories, getBookInfo} from "./js/api"
 
-getAllCategories()
-getTopBooks()
-getSeparateCategories("Business Books")
-getBookInfo("643282b1e85766588626a0b6")
