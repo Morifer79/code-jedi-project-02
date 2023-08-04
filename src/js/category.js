b1af08418b8f1f9c3bc6095c06fd62aa354e32f5
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-08-04 19:34:50 +0300

    update

--- src/js/category.js
+++ src/js/category.js
@@ -1,35 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const categoriesList = document.querySelectorAll('.categories li');
-  
-    categoriesList.forEach((category) => {
-      category.addEventListener('click', () => {
-        const selectedCategory = category.dataset.category;
-        renderContent(selectedCategory);
-        // console.log(selectedCategory);
-      });
+  const categoriesList = document.querySelectorAll('.categories li');
+
+  categoriesList.forEach((category) => {
+    category.addEventListener('click', () => {
+      const selectedCategory = category.dataset.category;
+      renderContent(selectedCategory);
+      console.log(selectedCategory);
     });
   });
+});
+
+async function renderContent(category) {
+  const contentContainer = document.querySelector('.content-container');
   
-  async function renderContent(category) {
-    const contentContainer = document.querySelector('.content-container');
-    // console.log(selectedCategory);
-    try {
-      const response = await fetchApiFunction(category);
-      const data = await response.json();
-// console.log(data);
-      let booksMarkup = '';
-      if (data.length === 0) {
-        booksMarkup = '<p>No books found in this category.</p>';
-      } else {
-        data.forEach(book => {
-          booksMarkup += createMarkup(book);
-        });
-      }
+  try {
+    const response = await fetchApiFunction(category);
+    const data = await response.json();
     
+    let booksMarkup = '';
+    if (data.length === 0) {
+      booksMarkup = '<p>No books found in this category.</p>';
+    } else {
+      data.forEach(book => {
+        booksMarkup += createMarkup(book);
+      });
+    }
+
+    const categoryMarkup = `<h2>${category}</h2>`;
+    contentContainer.innerHTML = categoryMarkup + booksMarkup;
+  } catch (error) {
+    console.error('Error fetching books:', error);
+    contentContainer.innerHTML = '<p>Error fetching books.</p>';
   }
-  
-  function createMarkup(book) {
-    return 
-    
-  }
+}
+
+function createMarkup(book) {
+  return `
+    <div class="book">
+      <img src="${book.book_image}" alt="${book.title}" />
+      <p>${book.title} by ${book.author}</p>
+    </div>
+  `;
 }
