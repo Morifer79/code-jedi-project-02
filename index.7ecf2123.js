340cba678d87cdd3f0cc5fb432cc4e76c2f1f166
Morifer79 <124445677+Morifer79@users.noreply.github.com>
2023-08-04 13:20:51 +0000

    Deploying to gh-pages from @ Morifer79/code-jedi-project-02@77b8e905d2a139aa7b6f441da959fbea29ca50f0 🚀

---
+++ index.7ecf2123.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".go-top");function o(){window.pageYOffset>document.documentElement.clientHeight?e.classList.add("go-top--show"):e.classList.remove("go-top--show")}function t(){window.pageYOffset>0&&(window.scrollBy(0,-75),setTimeout(t,0))}window.addEventListener("scroll",o),e.addEventListener("click",t),o(),t();
+//# sourceMappingURL=index.7ecf2123.js.map
