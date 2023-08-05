a2b8aa794a22e3937630c4ee4538a3f97d578c16
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-08-05 15:42:27 +0300

    Update category.js

--- src/js/category.js
+++ src/js/category.js
@@ -3,7 +3,6 @@ import { getSeparateCategories } from './api';
 const categoryLinks = document.querySelectorAll(".category-link");
 const booksContainer = document.querySelector(".books");
 
-
 categoryLinks.forEach(link => {
   link.addEventListener("click", function(event) {
     event.preventDefault();
@@ -28,28 +27,40 @@ function loadBooksByCategory(category) {
     lastWord,
     lastWordElement.outerHTML
   );
-    getSeparateCategories(category)
-      .then(response => {
-        const books = response.books; 
-        if (books.length > 0) {
-          const booksMarkup = books.map(book => createMarkup(book)).join("");
-          booksContainer.innerHTML = booksMarkup;
-        } else {
-          booksContainer.innerHTML = `<p>No found books in category "${category}".</p>`;
-        }
-      })
-      .catch(error => {
-        console.error(`Error loading books in category "${category}":`, error);
-      });
-  }
-  
-  function createMarkup(book) {
-    return `
-      <div class="book-card">
-        <img src="${book.book_image}" alt="${book.title}" class="book-image">
-        <h3 class="book-title">${book.title}</h3>
-        <p class="book-author">Author: ${book.author}</p>
-     </div>
-    `;
-  }
 
+  console.log(`Loading books in category "${category}"...`);
+
+  getSeparateCategories(category)
+    .then(response => {
+      console.log(`Books loaded successfully in category "${category}".`);
+      const books = response.books;
+
+      if (books.length > 0) {
+        console.table(books); 
+        const booksMarkup = books.map(book => createMarkup(book)).join("");
+        booksContainer.innerHTML = booksMarkup;
+      } else {
+        booksContainer.innerHTML = `<p>No found books in category "${category}".</p>`;
+      }
+    })
+    .catch(error => {
+      console.error(`Error loading books in category "${category}":`, error);
+    });
+}
+
+function createMarkup(book) {
+  return `
+    <div class="book-card">
+      <img src="${book.book_image}" alt="${book.title}" class="book-image">
+      <h3 class="book-title">${book.title}</h3>
+      <p class="book-author">Author: ${book.author}</p>
+    </div>
+  `;
+}
+console.log(categoryLinks);
+console.dir(categoryLinks);
+console.table(categoryLinks);
+
+console.log(booksMarkup);
+console.dir(booksMarkup);
+console.table(booksMarkup);
