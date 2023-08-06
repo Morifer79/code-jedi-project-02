ce4dfb92985daba2ae75487710f7d53c33075fcc
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-08-07 00:23:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import './js/api.js';
 import './js/go-top.js';
 import './js/start.js';
 import './js/theme.js';
-import './js/shopping-list.js';
-import './js/category.js';
 import './js/header.js'
