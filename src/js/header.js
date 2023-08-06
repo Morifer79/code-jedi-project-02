17e0179d4afce3b49ad20307cd6337c8798758be
YevhenBrovchuk <brovchukyevhen@ukr.net>
2023-08-07 02:45:16 +0300

    qwqwqwq

--- src/js/header.js
+++ src/js/header.js
@@ -1,19 +1,19 @@
-const elBtnUser = document.querySelector(".btn-user")
-let userName="User"
-elBtnUser.addEventListener("click", handleClick)
+// const elBtnUser = document.querySelector(".btn-user")
+// let userName="User"
+// elBtnUser.addEventListener("click", handleClick)
 
-console.log("Hello");
+// console.log("Hello");
 
-function handleClick() {
-    console.log(elBtnUser);
-    let marcup=`<svg width="37" height="37">
-                    <use href="./images/sprite.svg#user" class="test"></use>
-                </svg>
-                <span>${userName}</span>
-                <svg width="23" height="26">
-                    <use href="./images/caret-down.svg" class="test"></use>
-                </svg>`
-    elBtnUser.innerHTML = marcup;
-    // elBtnUser.style.backgroundColor='#4F2EE8'
-    console.log(elBtnUser);
-}
+// function handleClick() {
+//     console.log(elBtnUser);
+//     let marcup=`<svg width="37" height="37">
+//                     <use href="./images/sprite.svg#user" class="test"></use>
+//                 </svg>
+//                 <span>${userName}</span>
+//                 <svg width="23" height="26">
+//                     <use href="./images/caret-down.svg" class="test"></use>
+//                 </svg>`
+//     elBtnUser.innerHTML = marcup;
+//     // elBtnUser.style.backgroundColor='#4F2EE8'
+//     console.log(elBtnUser);
+// }
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,20 +1,20 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-menu]'),
-    closeModalBtn: document.querySelector('[data-modal-close-menu]'),
-    modal: document.querySelector('[data-modal-menu]'),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open-menu]'),
+//     closeModalBtn: document.querySelector('[data-modal-close-menu]'),
+//     modal: document.querySelector('[data-modal-menu]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    function toggleModal() {
-      console.log(refs.modal.classList.contains('is-hidden'));
-    // refs.modal.classList.toggle('is-hidden');
-    if (refs.modal.classList.contains('is-hidden')) {
-      document.body.style.overflow = '';
-    } else {
-      document.body.style.overflow = 'is-hidden';
-    }
-  }
-})();
+//     function toggleModal() {
+//       console.log(refs.modal.classList.contains('is-hidden'));
+//     // refs.modal.classList.toggle('is-hidden');
+//     if (refs.modal.classList.contains('is-hidden')) {
+//       document.body.style.overflow = '';
+//     } else {
+//       document.body.style.overflow = 'is-hidden';
+//     }
+//   }
+// })();
