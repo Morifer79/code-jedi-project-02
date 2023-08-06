443acc6961679f73f450a6584212bd6094c4391c
Yuliia Chaika <djuliya72@ukr.net>
2023-08-07 00:12:38 +0300

    added fn checkIcon

--- src/js/start.js
+++ src/js/start.js
@@ -2,10 +2,20 @@ const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
+  authForm: document.getElementById('authForm'),
+  signUpLink: document.querySelector('.sign-up-link'),
+  signInLink: document.querySelector('.sign-in-link'),
+  signUpBtn: document.querySelector('.signup-btn'),
+  // userIcon: document.querySelector('.js-user-icon'),
+  // cartIcon: document.querySelector('.js-shopping-cart-btn'),
+  //authUserIcon: document.querySelector('.js-authuser-icon'),
 };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.signUpLink.addEventListener('click', toggleAuthMode);
+refs.signInLink.addEventListener('click', toggleAuthMode);
+refs.authForm.addEventListener('submit', handleAuthFormSubmit);
 
 function toggleModal() {
   refs.modal.classList.toggle('is-hidden');
@@ -16,33 +26,21 @@ function toggleModal() {
   }
 }
 
-const authForm = document.getElementById('authForm');
-const signUpLink = document.querySelector('.sign-up-link');
-const signInLink = document.querySelector('.sign-in-link');
-const signUpBtn = document.querySelector('.signup-btn');
-
-signUpLink.addEventListener('click', toggleAuthMode);
-signInLink.addEventListener('click', toggleAuthMode);
-authForm.addEventListener('submit', handleAuthFormSubmit);
-
 function toggleAuthMode(event) {
   event.preventDefault();
-  signUpBtn.textContent = event.target.classList.contains('sign-up-link')
+  refs.signUpBtn.textContent = event.target.classList.contains('sign-up-link')
     ? 'SIGN UP'
     : 'SIGN IN';
 }
 
-let userName;
-
 function handleAuthFormSubmit(e) {
   e.preventDefault();
-  const isSignUp = signUpBtn.textContent === 'SIGN UP';
-  userName = e.currentTarget.elements.userName.value;
+  const isSignUp = refs.signUpBtn.textContent === 'SIGN UP';
+  const userName = e.currentTarget.elements.userName.value;
   const userEmail = e.currentTarget.elements.userEmail.value;
   const userPassword = e.currentTarget.elements.userPassword.value;
 
   if (isSignUp) {
-    // Реєстрація нового користувача
     const userData = {
       name: userName,
       email: userEmail,
@@ -51,11 +49,13 @@ function handleAuthFormSubmit(e) {
     const serializedUserData = JSON.stringify(userData);
     localStorage.setItem('userData', serializedUserData);
     console.log('Registration successful!');
-    // Notlify.Notlifix.success('Registration successful!');
-    setTimeout(toggleModal, 2000);
+    Notlify.Notlifix.success('Registration successful!');
+    setTimeout(() => {
+      toggleModal();
+      changeIcon();
+    }, 2000);
     e.target.reset();
   } else {
-    // Авторизація користувача
     const storedUserData = localStorage.getItem('userData');
     if (storedUserData) {
       const parsedUserData = JSON.parse(storedUserData);
@@ -65,14 +65,23 @@ function handleAuthFormSubmit(e) {
         parsedUserData.password === userPassword
       ) {
         console.log('Authorization successful!');
-        // Notlify.Notlifix.success('Authorization successful!');
-        setTimeout(toggleModal, 2000);
+        Notlify.Notlifix.success('Authorization successful!');
+        setTimeout(() => {
+          toggleModal();
+          changeIcon();
+        }, 2000);
         e.target.reset();
       } else {
         console.log('User not registered. Please register!');
-        // Notlify.Notlifix.error('User not registered. Please register!');
-        signUpBtn.textContent = 'SIGN UP';
+        Notlify.Notlifix.error('User is not registered! Please register!');
+        refs.signUpBtn.textContent = 'SIGN UP';
       }
     }
   }
 }
+
+// function changeIcon () {
+//       refs.authUserIcon.style.display = 'block';
+//       refs.cartIcon.style.display = 'block';
+//       refs.userIcon.style.display = 'none';
+// }
