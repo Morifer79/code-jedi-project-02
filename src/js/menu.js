670b614571c8c6487d42845bb544e1780f693cfc
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-08-07 22:43:43 +0300

    fix menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,8 @@ const refs = {
 	openModalBtnMenu: document.querySelector('[data-modal-open-menu]'),
 	closeModalBtnMenu: document.querySelector('[data-modal-close-menu]'),
 modalMenu: document.querySelector('[data-modal-menu]'),
+singUpBtn: document.querySelector(".btn-user"),
+menuGrupModal: document.querySelector(".menu-group-menu"),
 	
 };
 
@@ -14,11 +16,15 @@ refs.closeModalBtnMenu.addEventListener('click', toggleModalMenu);
 		refs.modalMenu.classList.toggle('is-hidden');
 if (!refs.modalMenu.classList.contains('is-hidden')) {
 	refs.openModalBtnMenu.style.display = "none";
- refs.closeModalBtnMenu.style.display="block"
+	refs.closeModalBtnMenu.style.display = "block"
+	refs.singUpBtn.style.display = "block"
+	refs.menuGrupModal.style.display="block"
 	document.body.style.overflow = 'hidden';
 } else {
 	refs.openModalBtnMenu.style.display = "block";
-	refs.closeModalBtnMenu.style.display="none"
+	refs.closeModalBtnMenu.style.display = "none";
+	refs.singUpBtn.style.display = "none"
+	refs.menuGrupModal.style.display="none"
 	document.body.style.overflow = '';
 	}
 };
