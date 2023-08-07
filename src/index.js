028e8ba23fcec0f9aae9e7b37b9bb94cf2d3d33b
YevhenBrovchuk <brovchukyevhen@ukr.net>
2023-08-07 10:58:08 +0300

    MobalMenu

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import './js/theme.js';
 // import './js/shopping-list.js';
 // import './js/category.js';
 // import './js/header.js';
-// import './js/menu.js';
+import './js/menu.js';
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,20 +1,27 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-menu]'),
-    closeModalBtn: document.querySelector('[data-modal-close-menu]'),
-    modal: document.querySelector('[data-modal-menu]'),
+const refs = {
+    openModalBtnMenu: document.querySelector('[data-modal-open-menu]'),
+    closeModalBtnMenu: document.querySelector('[data-modal-close-menu]'),
+  modalMenu: document.querySelector('[data-modal-menu]'),
+    
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnMenu.addEventListener('click', toggleModalMenu);
+  refs.closeModalBtnMenu.addEventListener('click', toggleModalMenu);
 
-    function toggleModal() {
-      console.log(refs.modal.classList.contains('is-hidden'));
-    // refs.modal.classList.toggle('is-hidden');
-    if (refs.modal.classList.contains('is-hidden')) {
-      document.body.style.overflow = '';
-    } else {
-      document.body.style.overflow = 'is-hidden';
+function toggleModalMenu() {
+ 
+      
+      refs.modalMenu.classList.toggle('is-hidden');
+  if (!refs.modalMenu.classList.contains('is-hidden')) {
+    refs.openModalBtnMenu.style.display = "none";
+   refs.closeModalBtnMenu.style.display="block"
+    document.body.style.overflow = 'hidden';
+  } else {
+    refs.openModalBtnMenu.style.display = "block";
+    refs.closeModalBtnMenu.style.display="none"
+    document.body.style.overflow = '';
     }
-  }
-})();
+    
+};
+  
+
