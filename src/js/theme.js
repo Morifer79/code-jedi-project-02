27b9deea6c9aa1abd9d907ca8576c15d61b17edc
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-08-09 22:38:19 +0300

    add cursor-pointer

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,14 +1,34 @@
-// const Theme = {
-// 	LIGHT: 'light-theme',
-// 	DARK: 'dark-theme',
-// }
+const Theme = {
+	LIGHT: 'light-theme',
+	DARK: 'dark-theme',
+}
 
-// const body = document.body;
-// const btn = document.querySelector('.theme-switch');
-// btn.addEventListener('click', changeTheme);
+const body = document.body;
+const btn = document.querySelector('.theme-switch');
+btn.addEventListener('change', changeTheme);
 
-// function changeTheme(e){
-// 	if(!e.currentTarget.checked){
+currentTheme();
 
-// 	}
-// }
+function changeTheme(e){
+	if(!e.currentTarget.checked){
+		body.classList.add(Theme.LIGHT);
+		body.classList.remove(Theme.DARK);
+		localStorage.setItem('theme', Theme.LIGHT)
+	} else {
+		body.classList.add(Theme.DARK);
+		body.classList.remove(Theme.LIGHT);
+		localStorage.setItem('theme', Theme.DARK)
+	}
+}
+
+function currentTheme(){
+	const savedTheme = localStorage.getItem('theme');
+	if(savedTheme){
+		body.classList.add(savedTheme);
+	} else {
+		body.classList.add(Theme.LIGHT);
+	}
+	if(savedTheme === Theme.DARK){
+		btn.checked = true;
+	}
+}
