fc89320137d136cc42d3fd65d209cf5275f9f0dc
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-08-09 16:55:17 +0300

    change theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,20 +2,25 @@ const themeSwitchers = document.querySelectorAll('.js-img-switcher');
 
 themeSwitchers.forEach(switcher => {
 	switcher.addEventListener('click', function () {
-		applyTheme(this.dataset.theme);
-		localStorage.setItem('theme', this.dataset.theme);
+		let theme = this.dataset.theme;
+		applyTheme(theme);
+		localStorage.setItem('theme', theme);
 	});
 });
 
 function applyTheme(themeName) {
-	let themeUrl = `./css/theme-${themeName}.css`;
+	let themeUrl = `/css/theme-${themeName}.css`;
 	document.querySelector('[title="theme"]').setAttribute('href', themeUrl);
+	themeSwitchers.forEach(button => {
+		button.style.display = 'block';
+	});
+	document.querySelector(`[data-theme="${themeName}"]`).style.display = 'none';
 }
 
 let activeTheme = localStorage.getItem('theme');
 
-if (activeTheme === null) {
-	applyTheme('dark');
+if (activeTheme === null || activeTheme === 'light') {
+	applyTheme('light');
 } else {
-	applyTheme(activeTheme);
+	applyTheme('dark');
 }
