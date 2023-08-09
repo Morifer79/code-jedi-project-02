0b932aa10af248bf6da399f894616eace9762f57
Vadym <v.ivanenko000@gmail.com>
2023-08-09 20:30:34 +0200

    bagFix

--- src/js/support-list.js
+++ src/js/support-list.js
@@ -1,57 +1,75 @@
 import saveTheChildren from '../images/01.png';
+import saveTheChildren_2x from '../images/save_children_x2.png';
 import projectHope from '../images/02.png';
+import projectHope_2x from '../images/hope_x2.png';
 import unitedeTwentyFour from '../images/07.png';
+import unitedeTwentyFour_2x from '../images/united_x2.png';
 import internationalMedicalCorps from '../images/03.png';
+import internationalMedicalCorps_2x from '../images/medical_corps_x2.png';
 import medecinsSansFrontiers from '../images/08.png';
+import medecinsSansFrontiers_2x from '../images/frontiers_x2.png';
 import razom from '../images/04.png';
+import razom_2x from '../images/razom_x2.png';
 import actionAgainstHunger from '../images/05.png';
+import actionAgainstHunger_2x from '../images/hanger_x2.png';
 import worldVision from '../images/09.png';
+import worldVision_2x from '../images/vision_x2.png';
 import sergiyPrytula from '../images/06.png';
+import sergiyPrytula_2x from '../images/prytula_x2.png';
 
 export const supportArr = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
     img: saveTheChildren,
+    img2x: saveTheChildren_2x,
   },
   {
     title: 'Project Hope',
     url: 'https://www.projecthope.org/',
     img: projectHope,
+    img2x: projectHope_2x,
   },
   {
     title: 'United 24',
     url: 'https://u24.gov.ua/',
     img: unitedeTwentyFour,
+    img2x: unitedeTwentyFour_2x,
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/',
     img: internationalMedicalCorps,
+    img2x: internationalMedicalCorps_2x,
   },
   {
     title: 'Medecins Sans Frontiers',
     url: 'https://www.msf.org/',
     img: medecinsSansFrontiers,
+    img2x: medecinsSansFrontiers_2x,
   },
   {
     title: 'Razom',
     url: 'https://www.razomforukraine.org/',
     img: razom,
+    img2x: razom_2x,
   },
   {
     title: 'Action Against Hunger',
     url: 'https://www.actionagainsthunger.org/',
     img: actionAgainstHunger,
+    img2x: actionAgainstHunger_2x,
   },
   {
     title: 'World Vision',
     url: 'https://www.worldvision.org/',
     img: worldVision,
+    img2x: worldVision_2x,
   },
   {
     title: 'Charity Foundation SergiY Prytula',
     url: 'https://prytulafoundation.org/en',
     img: sergiyPrytula,
+    img2x: sergiyPrytula_2x,
   },
 ];
