47b433925e148994f657b9b1c3c13497d50d59ee
Morifer <124445677+Morifer79@users.noreply.github.com>
2023-08-09 23:48:14 +0300

    fix header & menu

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,12 @@
 const elBtnUser = document.querySelector(".btn-user-help");
 
  const elBtnUserMob=document.querySelector(".btn-user-helper")
-// const elBtnTheme = document.querySelector(".btn-theme");
+
 import {userName} from './start.js';
-// let userName="User"
+
 elBtnUser.addEventListener("click", handleClick);
 elBtnUserMob.addEventListener("click", handleClick)
-// elBtnTheme.addEventListener("click", switcherClick)
+
 
 export function handleClick() {
   if (userName??undefined) {
@@ -19,5 +19,8 @@ export function handleClick() {
                     <use href="./images/caret-down.svg" class="test"></use>
                 </svg>`
     elBtnUser.innerHTML = marcup;
+    elBtnUserMob.innerHTML = marcup;
+    elBtnUser.style.pointerEvents = "none";
+    elBtnUserMob.style.pointerEvents = "none";
   }
 }
